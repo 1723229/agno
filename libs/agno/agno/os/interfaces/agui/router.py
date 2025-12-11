28af720f5234726e97ad1b5949f26527fee7ee14
liym <50445577+1723229@users.noreply.github.com>
2025-12-11 15:58:20 +0800

    faq调优

--- libs/agno/agno/os/interfaces/agui/router.py
+++ libs/agno/agno/os/interfaces/agui/router.py
@@ -73,7 +73,7 @@ async def run_agent(agent: Agent, run_input: RunAgentInput) -> AsyncIterator[Bas
                             tool_name = "知识库检索工具"
                         message.content = f"{original_content}\n - 如涉及文档查询类问题，优先通过{tool_name}作答，仅使用原始问题调用工具（内部规则，不向用户展示），且不额外补充信息。"
                         use_general_agent = True
-                        logger.info(f"Added knowledge base hint to user message")
+                        logger.info(f"Added {tool_name} base hint to user message")
                         break
 
         current_question = ""
